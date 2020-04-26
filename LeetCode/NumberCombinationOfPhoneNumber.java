bd979d01ba0e75081be5245dc625fe99c28b1144
ericgerry <ericgerry@126.com>
2020-04-26 23:06:26 +0800

    电话号码的字母组合和盛最多水的容器

---
+++ LeetCode/NumberCombinationOfPhoneNumber.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author：Eric
+ * @Date：2020/4/26 22:34
+ */
+
+/*
+* 电话号码的字母组合
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+示例:
+
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].*/
+
+public class NumberCombinationOfPhoneNumber {
+    private String letterMap[] = {
+            " ",    //0
+            "",     //1
+            "abc",  //2
+            "def",  //3
+            "ghi",  //4
+            "jkl",  //5
+            "mno",  //6
+            "pqrs", //7
+            "tuv",  //8
+            "wxyz"  //9
+    };
+
+    private ArrayList<String> res;
+
+    public List<String> letterCombinations(String digits) {
+
+        res = new ArrayList<String>();
+        if(digits.equals("")) {
+            return res;
+        }
+
+        findCombination(digits, 0, "");
+        return res;
+    }
+
+    private void findCombination(String digits, int index, String s){
+
+        if(index == digits.length()){
+            res.add(s);
+            return;
+        }
+
+        Character c = digits.charAt(index);
+        String letters = letterMap[c - '0'];
+        for(int i = 0 ; i < letters.length() ; i ++){
+            findCombination(digits, index+1, s + letters.charAt(i));
+        }
+        return;
+    }
+
+}
