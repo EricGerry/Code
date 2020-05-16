a2fc8ab81a317993d05df2d63fea074d5d72c262
ericgerry <ericgerry@126.com>
2020-05-16 22:46:58 +0800

    平衡二叉树和二叉树的最小深度

---
+++ LeetCode/BalancedBinaryTree.java
@@ -0,0 +1,56 @@
+/**
+ * @version 2020.1
+ * @Author Eric
+ * @date 2020/5/16 22:35
+ */
+
+/*
+* 110. 平衡二叉树
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+示例 1:
+
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+
+示例 2:
+
+给定二叉树 [1,2,2,3,3,null,null,4,4]
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+返回 false 。
+
+ */
+
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        return Math.abs(height(root.left) - height(root.right)) < 2 && isBalanced(root.left)
+                && isBalanced(root.right);
+    }
+    private int height(TreeNode node){
+        if (node == null){
+            return 0;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
+
+    }
+}
